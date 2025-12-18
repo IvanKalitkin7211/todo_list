5e948ea9a5452c39a14f1164008943636ff5429b
juggernaut <normalniydadawade@gmail.com>
2025-12-19 04:54:05 +0700

    add middleware auth

---
+++ internal/api/middleware/auth.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"strings"
+)
+
+func AuthMiddleware(secret string) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			authHeader := c.Request().Header.Get("Authorization")
+			if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+				return c.JSON(http.StatusUnauthorized, map[string]string{"error": "missing or invalid token"})
+			}
+
+			tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+			token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+				return []byte(secret), nil
+			})
+
+			if err != nil || !token.Valid {
+				return c.JSON(http.StatusUnauthorized, map[string]string{"error": "unauthorized"})
+			}
+
+			claims, ok := token.Claims.(jwt.MapClaims)
+			if !ok {
+				return c.JSON(http.StatusUnauthorized, map[string]string{"error": "invalid token claims"})
+			}
+
+			// Устанавливаем user_id в контекст для использования в хендлерах
+			c.Set("user_id", claims["sub"])
+			return next(c)
+		}
+	}
+}
